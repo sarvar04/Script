2c54042b547b02216297ba3d76a626c525d0318b
Sarvar Nadaf <45162347+sarvar04@users.noreply.github.com>
2018-12-13 09:52:16 +0530

    Add files via upload

--- sarvar.sh
+++ sarvar.sh
@@ -32,9 +32,64 @@ sudo echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> /usr/local/had
 sudo echo "export HADOOP_OPTS=-Djava.net.preferIPV4Stack=true" >> /usr/local/hadoop/conf/hadoop-env.sh
 . /usr/local/hadoop/conf/hadoop-env.sh
 
-sudo sed -i -e '/<configuration>/a\<property><name>fs.default.name</name>\n<value>hdfs://localhost:9000</value></property>\n<property><name>hadoop.tmp.dir</name>\n<value>/usr/local/hadoop/tmp</value></property>\n' /usr/local/hadoop/conf/core-site.xml
-sudo sed -i -e '/<configuration>/a\<property><name>dfs.replication</name>\n<value>1</value></property>' /usr/local/hadoop/conf/hdfs-site.xml
-sudo sed -i -e '/<configuration>/a\<property><name>mapred.job.tracker</name>\n<value>hdfs://localhost:9001</value></property>' /usr/local/hadoop/conf/mapred-site.xml
+cd ~
+
+touch core-site.xml
+
+echo '?^?^?<?xml version="1.0"?>
+<?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
+
+<!-- Put site-specific property overrides in this file. -->
+
+<configuration>
+<property>
+<name>fs.default.name</name>
+<value>hdfs://localhost:9000</value>
+</property>
+
+<property>
+<name>hadoop.tmp.dir</name>
+<value>/usr/local/hadoop/tmp</value>
+</property>
+</configuration>' >> core-site.xml
+
+sudo mv core-site.xml /usr/local/hadoop/conf/
+
+
+
+touch hdfs-site.xml
+
+echo '<?xml version="1.0"?>
+<?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
+
+<!-- Put site-specific property overrides in this file. -->
+
+<configuration>
+<property>
+<name>dfs.replication</name>
+<value>1</value>
+</property>
+</configuration>' >> hdfs-site.xml
+
+sudo mv hdfs-site.xml /usr/local/hadoop/conf/
+
+
+
+touch mapred-site.xml
+
+echo '<?xml version="1.0"?>
+<?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
+
+<!-- Put site-specific property overrides in this file. -->
+
+<configuration>
+<property>
+<name>mapred.job.tracker</name>
+<value>hdfs://localhost:9001</value>
+</property>
+</configuration>' >> mapred-site.xml
+
+sudo mv mapred-site.xml /usr/local/hadoop/conf/
 
 hadoop namenode -format
 
